06f1d81933fbc7976ec067fefd0e265a49580654
Sage Kaplan <sagek@icloud.com>
2026-09-01 20:06:44 -0700

    1.3 parser and filereader

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,47 +1,39 @@
 import csv
 
 
-class Student:
-    def __init__(
-        self,
-        last_name,
-        first_name,
-        grade,
-        classroom,
-        bus,
-        gpa,
-        teacher_last,
-        teacher_first
-    ):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.grade = grade
-        self.classroom = classroom
-        self.bus = bus
-        self.gpa = gpa
-        self.teacher_last = teacher_last
-        self.teacher_first = teacher_first
+class Student:                                                              #student class
+    def __init__(self, last_name, first_name, grade, classroom,             #class initializer
+                 bus, gpa, teacher_last, teacher_first):
+        self.last_name = last_name                                          #last name
+        self.first_name = first_name                                        #first name
+        self.grade = grade                                                  #grade level
+        self.classroom = classroom                                          #classroom number
+        self.bus = bus                                                      #bus route number
+        self.gpa = gpa                                                      #GPA
+        self.teacher_last = teacher_last                                    #teacher last name
+        self.teacher_first = teacher_first                                  #teacher first name
 
 
-def parse_students(filename):
-    students = []
+def parse_students(filename):                                               #reads file and creates object for each student
+    students = []                                                           #list of student objects
 
-    with open(filename, "r") as file:
-        for line in file:
-            fields = line.strip().split(",")
+    with open(filename, "r") as file:                                       #opens and reads student file
+        for line in file:                                                   #loops through each line in file
+            fields = line.strip().split(",")                                #formats data for student objects
 
-            student = Student(
-                fields[0].strip(),
-                fields[1].strip(),
-                int(fields[2]),
-                int(fields[3]),
-                int(fields[4]),
-                float(fields[5]),
-                fields[6].strip(),
-                fields[7].strip()
+            student = Student(                                              #creates student object
+                fields[0].strip(),                                          #student last name
+                fields[1].strip(),                                          #student first name
+                int(fields[2]),                                             #student grade as int
+                int(fields[3]),                                             #classroom number as int
+                int(fields[4]),                                             #bus route as int
+                float(fields[5]),                                           #student GPA as float
+                fields[6].strip(),                                          #teacher last name
+                fields[7].strip()                                           #teachers first name
             )
 
-            students.append(student)
+            students.append(student)                                        #appends student object to student list
 
-    return students
+    return students                                                         #returns list of student objects
+
 
